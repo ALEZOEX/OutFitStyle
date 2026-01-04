b1ca44b63877b0838bbea1ad77ced57fb6e17160
ALEZOEX <khaival2203@gmail.com>
2026-01-05 00:41:13 +0300

    удалены лишние файлы и обновлен .gitignore

--- cleanup.ps1
+++
@@ -1,3 +0,0 @@
-# Освобождение места на диске
-Get-Process | Where-Object {$_.Path -like "*full_server_code*"} | Stop-Process -Force
-Remove-Item "C:\Users\Admin\GolandProjects\outfitstyle\server\full_server_code*.txt" -Force
