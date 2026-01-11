1b71958be8d070b58cdd26e20d8a695f44ab7b06
ALEZOEX <khaival2203@gmail.com>
2026-01-11 19:33:33 +0300

    Рефакторинг: платформозависимые флаги, определение хоста API и условный импорт

---
+++ client/lib/app/platform_flags.dart
@@ -0,0 +1,4 @@
+/// Заглушка для условного импорта
+bool get isAndroid => throw UnsupportedError('Platform-specific implementation required');
+
+bool get isIOS => throw UnsupportedError('Platform-specific implementation required');
