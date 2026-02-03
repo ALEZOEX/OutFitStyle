b6458d7891dd8bd2856bfec96a39457659cc7316
Senior Engineer <engineer@outfitstyle.ai>
2026-02-03 22:09:32 +0300

    feat: add missing TimelineDay entity class

---
+++ client/lib/data/local/dao/timeline_models.dart
@@ -0,0 +1,15 @@
+import '../../../domain/entities/timeline_entity.dart';
+import '../../../domain/entities/recommendation_entity.dart';
+
+// Вспомогательный класс для DAO
+class TimelineDayRow {
+  final DateTime date;
+  final int outfitCount;
+  final RecommendationRow? recommendation;
+
+  const TimelineDayRow({
+    required this.date,
+    required this.outfitCount,
+    this.recommendation,
+  });
+}
