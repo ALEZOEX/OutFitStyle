31e0c2f657dc536e83fffa8e4a9358845637eafa
ALEZOEX <khaival2203@gmail.com>
2026-02-03 20:39:53 +0300

    fix(client): implement RecommendationRow class and fix related type issues

--- client/lib/features/recommendations/presentation/recommendation_detail_screen.dart
+++ client/lib/features/recommendations/presentation/recommendation_detail_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../app/di.dart';
+import '../../../domain/entities/recommendation_entity.dart';
 import '../../../ui/atoms/haptics.dart';
 import '../../../ui/atoms/outfit_app_bar.dart';
 import '../../../ui/atoms/skeleton.dart';
