8b8b7e602e859ae38d731f55ed2aa8e3c09c4936
ALEZOEX <khaival2203@gmail.com>
2026-02-25 00:34:20 +0300

    fix: убрать неиспользуемый импорт в main.dart

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'src/presentation/app/app.dart';
-import 'src/core/error/error_handler.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
