74eeed83f12ddd0286765bcb108eeb236146178a
ALEZOEX <khaival2203@gmail.com>
2026-04-03 19:43:18 +0300

    migration: add missing columns to recommendation_items (source, is_from_wardrobe, alternatives_json)

--- server/migrations/000026_add_source_to_recommendation_items.down.sql
+++ server/migrations/000026_add_source_to_recommendation_items.down.sql
@@ -1,2 +1,8 @@
 ALTER TABLE recommendation_items
   DROP COLUMN IF EXISTS source;
+
+ALTER TABLE recommendation_items
+  DROP COLUMN IF EXISTS is_from_wardrobe;
+
+ALTER TABLE recommendation_items
+  DROP COLUMN IF EXISTS alternatives_json;
--- server/migrations/000026_add_source_to_recommendation_items.up.sql
+++ server/migrations/000026_add_source_to_recommendation_items.up.sql
@@ -1,2 +1,8 @@
 ALTER TABLE recommendation_items
   ADD COLUMN IF NOT EXISTS source TEXT;
+
+ALTER TABLE recommendation_items
+  ADD COLUMN IF NOT EXISTS is_from_wardrobe BOOLEAN DEFAULT false;
+
+ALTER TABLE recommendation_items
+  ADD COLUMN IF NOT EXISTS alternatives_json JSONB;
